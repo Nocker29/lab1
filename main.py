93f23fdb25fd492f0233ba4822d78260ff56921d
Nocker29 <49701710+Nocker29@users.noreply.github.com>
2021-02-22 14:00:48 +0100

    Wykonanie zadania

--- main.py
+++ main.py
@@ -1,34 +1,10 @@
-from math import *
-
-a = 10
-b = 5
+# zad1
+a = 'napis'
+b = 'napis2'
 c = 2
-
-dzielenie = b/c
-print(dzielenie)
-dzielenie_calkowite = b // c
-print(dzielenie_calkowite)
-
-dodawanie = a+b
-potega = a ** c
-print(dodawanie)
-print(potega)
-
-potega = pow(b,c)
-print(potega)
-
-print(sqrt(9))
-
-print(pi)
-
-imie = "Radek"
-wiek = 27
-
-print("mam na imie %s i mam %d lat" % (imie, wiek))
-
-print("wynik dzialania jest rowny a=%(zm)d" % {'zm':12})
-
-x = 5
-y = 3
-z = 5-3
-print("wynik dzialania %(z1)d-%(z2)d=%(z3)d" % {'z1':x, 'z2':y, 'z3':z})
+d = 3
+e = 4.5
+f = 3.5
+g = (3+2j)
+h = (2+2j)
+print(a,b,c,d,e,f,g,h)

a8cb9d55c2303b50f462dc9faa419f21759495cd
Nocker29 <49701710+Nocker29@users.noreply.github.com>
2021-02-22 14:23:30 +0100

    zad2

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-# zad1
-a = 'napis'
-b = 'napis2'
-c = 2
-d = 3
-e = 4.5
-f = 3.5
-g = (3+2j)
-h = (2+2j)
-print(a,b,c,d,e,f,g,h)
+# zad2
+a = 8
+b = 3
+dodawanie = a+b
+odejmowanie = a-b
+mnozenie = a*b
+dzielenie = a/b
+print(dodawanie, odejmowanie, mnozenie, dzielenie)
